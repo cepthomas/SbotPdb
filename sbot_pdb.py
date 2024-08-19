65bc3605c268d3cc09f7ba67f7dc08a5e794328c
cepthomas <cepthomas@gmail.com>
2024-08-19 12:59:51 -0600

    WIP.

--- sbot_pdb.py
+++ sbot_pdb.py
@@ -3,6 +3,7 @@ import os
 import re
 import socket
 import sys
+import traceback
 import sublime
 import sublime_plugin
 from pdb import Pdb
@@ -24,15 +25,16 @@ ANSI_RESET  = '\033[0m'
 EOL = '\r\n'
 
 
-# run: "C:\Program Files\PuTTY\kitty-0.76.1.13.exe" -load "sbot_dev"
-# TODO1 make it easier to run and Close+restart.
-# https://the.earth.li/~sgtatham/putty/0.81/htmldoc/Chapter3.html#using-cmdline
-# https://www.9bis.net/kitty/#!pages/CommandLine.md
-
-# TODO1 Unhandled exception BdbQuit when q(uit) not c(ont). https://stackoverflow.com/a/34936583
-
-# TODO? Do not bind to a specific port. Instead, bind to port 0.
-
+# TODO Unhandled exception BdbQuit when using q(uit) not c(ont). Maybe fix/patch.
+# https://stackoverflow.com/a/34936583:
+# If you continue from the (pdb) prompt and allow your code to finish normally, I wouldn't expect
+# output like the traceback you indicated, but if you quit pdb, with the quit command or ^D (EOF),
+# a traceback like that occurs because there is nothing to catch the BdbQuit exception raised when
+# the debugger quits. In bdb.py self.quitting gets set to True by the set_quit method (and by finally
+# clauses in the various run methods). Dispatch methods called by trace_dispatch raise BdbQuit when
+# self.quitting is True, and the typical except: clause for BdbQuit is a simple pass statement; pdb
+# inherits all of that from gdb. In short, exception handling is used to disable the system trace function
+# used by the debugger, when the debugger interaction finishes early.
 
 
 #-----------------------------------------------------------------------------------
@@ -115,58 +117,88 @@ class FileWrapper(object):
                 self._send(f'(Pdb) ')
 
 
+
+
+
+# TODO1 harmonize print(), log(), write_debug(), ...
+
 #-----------------------------------------------------------------------------------
-class StPdb(Pdb):
+class SbotPdb(Pdb):
     '''Run pdb behind a blocking telnet server.'''
     active_instance = None
 
     def __init__(self):
         settings = sublime.load_settings(SBOTPDB_SETTINGS_FILE)
-        host = settings.get('host')
-        port = settings.get('port')
-        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-        listen_socket.bind((host, port))
-        # print(f'StPdb session open at {listen_socket.getsockname()}, waiting for connection.')
-        listen_socket.listen(1)  # TODO1 need a timeout/retry mechanism.
-        conn, address = listen_socket.accept()
-        # print(f'StPdb session accepted connection from {repr(address)}.')
-        self.handle = FileWrapper(conn)
-        Pdb.__init__(self, completekey='tab', stdin=self.handle, stdout=self.handle)
-        StPdb.active_instance = self
 
-    def __restore(self):
-        self.handle.close()
-        StPdb.active_instance = None
-
-    def set_trace(self, frame=None):
-        if frame is None:
-            frame = sys._getframe().f_back
         try:
-            Pdb.set_trace(self, frame)
-        except IOError as e:
-            if e.errno == errno.ECONNRESET:  # TODO1 retry?
-                pass
-            else:
-                self.do_error(e.__traceback__)
+            host = settings.get('host')
+            port = settings.get('port')
+            self.handle = None
+            lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            lsock.settimeout(1)  # Seconds.
+            lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+            lsock.bind((host, port))
+            # print(f'SbotPdb session open at {lsock.getsockname()}, waiting for connection.')
+            lsock.listen(1)
+            conn, address = lsock.accept()
+            # print(f'SbotPdb session accepted connection from {repr(address)}.')
+            self.handle = FileWrapper(conn)
+            Pdb.__init__(self, completekey='tab', stdin=self.handle, stdout=self.handle)
+            SbotPdb.active_instance = self
+        except socket.timeout as e:
+            # Timeout waiting for a client to connect. TODO1 retry etc.
+            pass
         except Exception as e:
-            self.do_error(e.__traceback__)
+            print('444', type(e))
+            self.do_error(e)
 
-    def do_error(self, tb):
-        self.handle.write_debug(f'Exception! {traceback.format_tb(tb)}')
+    def set_trace(self, frame):
+        # if frame is None:
+        #     frame = sys._getframe().f_back
+
+        try:
+            # print('777', type(frame), dir(frame)) #777 <class 'frame'> ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'clear', 'f_back', 'f_builtins', 'f_code', 'f_globals', 'f_lasti', 'f_lineno', 'f_locals', 'f_trace', 'f_trace_lines', 'f_trace_opcodes']
+            # Pdb.set_trace(frame)  # This blocks until user says done.
+            Pdb.set_trace(self, frame)  # This blocks until user says done.
+            # super().set_trace(frame)  # This blocks until user says done.      # calls base class method
+
+        except IOError as e:
+            if e.errno == errno.ECONNRESET:  # Client closed the connection.
+                print(f'111')
+                pass  # TODO1 reopen/retry?
+            else:
+                print(f'122')
+                self.do_error(e)
+        except Exception as e:
+            print(f'133', type(e))  # here AttributeError
+            self.do_error(e)
+
+    def do_error(self, e):
+        tb = e.__traceback__
+        s = '\n'.join(traceback.format_tb(tb))
+        if self.handle is not None:
+            self.handle.write_debug(f'222 Exception! {e}\n{s}')
+        else:
+            print(f'333 Exception! {e}\n{s}') #333 Exception! 'frame' object has no attribute 'reset'
+
         # frame = traceback.extract_tb(tb)[-1]
         # sublime.error_message(f'Exception at {frame.name}({frame.lineno})')
         self.do_quit()
 
     def do_quit(self, arg=None):
-        self.__restore()
-        return Pdb.do_quit(self, arg)
+        if self.handle is not None:
+            self.handle.close()
 
-    # do_q = do_exit = do_quit  # TODO what?
+        SbotPdb.active_instance = None
+
+        try:
+            res = Pdb.do_quit(self, arg) # <<< exc
+        except Exception as e:
+            print('111', '\n'.join(traceback.format_tb(e.__traceback__)))
 
 
 #-----------------------------------------------------------------------------------
 def set_trace():
-    '''Opens a remote PDB using import stpdb; stpdb.set_trace() syntax.'''
-    rdb = StPdb()
-    rdb.set_trace(frame=sys._getframe().f_back)
+    '''Opens a remote PDB using familiar syntax.'''
+    rdb = SbotPdb()
+    rdb.set_trace(sys._getframe().f_back)
--- test_sbot_pdb.py
+++ test_sbot_pdb.py
@@ -10,25 +10,6 @@ from . import sbot_pdb
 # For reload scenario.
 importlib.reload(sbot_pdb)
 
-# TODO1 get this running.
-
-
-# elif what == 'stpdb':
-#     # run: "C:\Program Files\PuTTY\kitty-0.76.1.13.exe" -load "sbot_dev"
-#     # make it easier to run and Close+restart.
-#     # https://the.earth.li/~sgtatham/putty/0.81/htmldoc/Chapter3.html#using-cmdline
-#     # https://www.9bis.net/kitty/#!pages/CommandLine.md
-
-#     # Unhandled exception BdbQuit when q(uit) not c(ont). https://stackoverflow.com/a/34936583
-
-#     from . import stpdb
-#     try:
-#         # stpdb.StPdb()  # shorter
-#         stpdb.set_trace()
-#         # stpdb.StPdb('127.0.0.1', 4444).set_trace()
-#     except Exception as e:
-#         dir(e)
-#         sc.error(f'StPdb exception: {e}')
 
 
 #-----------------------------------------------------------------------------------
@@ -38,7 +19,7 @@ class SbotPdbTestCommand(sublime_plugin.TextCommand):
 
 
 #----------------------------------------------------------
-class TestClass(object):
+class MyClass(object):
 
     def __init__(self, name, tags, arg):
         self._name = name
@@ -49,34 +30,36 @@ class TestClass(object):
         res = f'{self._arg}-user-{arg}'
         return res
 
-    def do_class_exception(self, arg):
+    def class_boom(self, arg):
         x = 1 / 0  # boom
 
 
 #----------------------------------------------------------
-def a_test_function(a1: int, a2: float):
-    cl1 = TestClass('number 1', [45, 78, 23], a1)
-    cl2 = TestClass('number 2', [100, 101, 102], a2)
+def function_1(a1: int, a2: float):
+    cl1 = MyClass('number 1', [45, 78, 23], a1)
+    cl2 = MyClass('number 2', [100, 101, 102], a2)
     ret = f'answer is cl1:{cl1.do_something(a1)}...cl2:{cl2.do_something(a2)}'
-    # ret = f'{cl1.do_class_exception(a2)}'
+    # ret = f'{cl1.class_boom(a2)}'
     return ret
 
 
 #----------------------------------------------------------
-def another_test_function(a_list, a_dict):
+def function_2(a_list, a_dict):
     return len(a_list) + len(a_dict)
 
 
 #----------------------------------------------------------
-def test_exception_function():
+def function_boom():
     i = 0
     return 1 / i  # boom
 
 
 #----------------------------------------------------------
 def do_a_suite(alpha, number):
+
     sbot_pdb.set_trace()
-    ret = a_test_function(5, 9.126)
-    # test_exception_function()
-    ret = another_test_function([33, 'thanks', 3.56], {'aaa': 111, 'bbb': 222, 'ccc': 333})
+
+    ret = function_1(5, 9.126)
+    # function_boom()
+    ret = function_2([33, 'thanks', 3.56], {'aaa': 111, 'bbb': 222, 'ccc': 333})
     return ret
