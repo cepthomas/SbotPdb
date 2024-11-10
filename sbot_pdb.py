a37f39772a41d799af1aa7a8d4cbc32cb8ce0d13
cepthomas <cepthomas@gmail.com>
2024-11-10 14:38:43 -0600

    Simplifying.

--- sbot_pdb.py
+++ sbot_pdb.py
@@ -1,22 +1,37 @@
 import sys
 import socket
 import pdb
+import json
+import os
+import datetime
 import sublime
-import sublime_plugin
-from . import sbot_common as sc
 
 
 # Delimiter for socket message lines.
-COMM_DELIM = '\n'
+_comm_delim = '\n'
 
-# Delimiter for log lines.
-LOG_EOL = '\n'
+# Configuration.
+_config = None
+
+# Where to log.
+_log_fn = '???'
 
 
 #-----------------------------------------------------------------------------------
 def plugin_loaded():
-    '''Called per plugin instance.'''
-    sc.debug(f'plugin_loaded() {__package__}')
+    '''Read the config.'''
+    global _config, _log_fn
+    dir, _ = os.path.split(__file__)
+    fn = os.path.join(dir, 'config.json')
+
+    try:
+        with open(fn, 'r') as fp:
+            _config = json.load(fp)
+            # print(_config)
+            _log_fn = os.path.join(dir, 'sbot_pdb.log')
+    except Exception as e:
+        # No logging yet.
+        sublime.message_dialog(f'Error reading config file {fn}: {e}')
 
 
 #-----------------------------------------------------------------------------------
@@ -46,7 +61,7 @@ class CommIf(object):
         return self.stream.encoding
 
     def send(self, s):
-        # sc.debug(f'send(): {make_readable(s)}')
+        # log(f'DBG send(): {make_readable(s)}')
         self.conn.sendall(s.encode())
 
     def readline(self, size=1):
@@ -55,15 +70,15 @@ class CommIf(object):
         try:
             s = self.stream.readline()
             self.last_cmd = s
-            # sc.debug(f'Received command: {make_readable(s)}')
+            # log(f'DBG Received command: {make_readable(s)}')
             return self.last_cmd
 
         except (ConnectionError, socket.timeout) as e:
-            sc.debug(f'Disconnected: {type(e)}')
+            log(f'DBG Disconnected: {type(e)}')
             raise
 
         except Exception as e:
-            sc.debug(f'CommIf.readline() other exception: {str(e)}')
+            log(f'DBG CommIf.readline() other exception: {str(e)}')
             self.buff = ''
             raise
 
@@ -76,21 +91,21 @@ class CommIf(object):
             # pdb writes lines piecemeal but we want full proper lines.
             # Easiest is to accumulate in a buffer until we see the prompt then slice and write.
             if '(Pdb)' in line:
-                settings = sublime.load_settings(sc.get_settings_fn())
+                # settings = sublime.load_settings(sc.get_settings_fn())
 
                 for s in self.buff.splitlines():
-                    # sc.debug(f'Send response: {s}')
+                    # sc.debug(f'DBG Send response: {s}')
                     color = None
 
-                    if settings.get('use_color'):
-                        if s.startswith('-> '): color = settings.get('current_line_color')
-                        elif ' ->' in s: color = settings.get('current_line_color')
-                        elif s.startswith('>> '): color = settings.get('exception_line_color')
-                        elif '***' in s: color = settings.get('error_color')
-                        elif 'Error:' in s: color = settings.get('error_color')
-                        elif s.startswith('> '): color = settings.get('stack_location_color')
+                    if _config['use_color']:
+                        if s.startswith('-> '): color = _config['current_line_color']
+                        elif ' ->' in s: color = _config['current_line_color']
+                        elif s.startswith('>> '): color = _config['exception_line_color']
+                        elif '***' in s: color = _config['error_color']
+                        elif 'Error:' in s: color = _config['error_color']
+                        elif s.startswith('> '): color = _config['stack_location_color']
 
-                    self.send(f'{s}{COMM_DELIM}' if color is None else f'\033[{color}m{s}\033[0m{COMM_DELIM}')
+                    self.send(f'{s}{_comm_delim}' if color is None else f'\033[{color}m{s}\033[0m{_comm_delim}')
 
                 self.writePrompt()
 
@@ -101,25 +116,27 @@ class CommIf(object):
                 self.buff += line
 
         except (ConnectionError, socket.timeout) as e:
-            sc.debug(f'Disconnected: {type(e)}')
+            log(f'Disconnected: {type(e)}')
+            # sc.debug(f'Disconnected: {type(e)}')
             raise
 
         except Exception as e:
-            sc.debug(f'CommIf.write() other exception: {str(e)}')
+            log(f'CommIf.write() other exception: {str(e)}')
+            # sc.debug(f'CommIf.write() other exception: {str(e)}')
             self.buff = ''
             raise
 
     def writeInfo(self, line):
         '''Write internal non-pdb info to client.'''
-        settings = sublime.load_settings(sc.get_settings_fn())
-        ind = settings.get('internal_message_ind')
-        self.send(f'{ind} {line}{COMM_DELIM}')
+        # settings = sublime.load_settings(sc.get_settings_fn())
+        ind = _config['internal_message_ind']
+        self.send(f'{ind} {line}{_comm_delim}')
         self.writePrompt()
 
     def writePrompt(self):
-        settings = sublime.load_settings(sc.get_settings_fn())
-        pc = settings.get('prompt_color')
-        s = f'\033[{pc}m(Pdb)\033[0m ' if settings.get('use_color') else '(Pdb) '
+        # settings = sublime.load_settings(sc.get_settings_fn())
+        pc = _config['prompt_color']
+        s = f'\033[{pc}m(Pdb)\033[0m ' if _config['use_color'] else '(Pdb)'
         self.send(s)
 
 
@@ -134,43 +151,48 @@ class SbotPdb(pdb.Pdb):
             self.commif = None
             self.active_instance = None
 
-            settings = sublime.load_settings(sc.get_settings_fn())
-            host = str(settings.get('host'))
-            port = int(str(settings.get('port')))
-            client_connect_timeout = int(str(settings.get('client_connect_timeout')))
+            # settings = sublime.load_settings(sc.get_settings_fn())
+            host = _config['host']
+            port = _config['port']
+            client_connect_timeout = _config['client_connect_timeout']
 
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if client_connect_timeout > 0:
                 self.sock.settimeout(client_connect_timeout)  # Seconds.
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
             self.sock.bind((host, port))
-            sc.info(f'Server started on {host}:{port} - waiting for connection.')
+            log(f'Server started on {host}:{port} - waiting for connection.')
+            # sc.info(f'Server started on {host}:{port} - waiting for connection.')
 
             # Blocks until client connect or timeout.
             self.sock.listen(1)
             conn, address = self.sock.accept()
 
             # Connected.
-            sc.info(f'Server accepted connection from {repr(address)}.')
+            log(f'Server accepted connection from {repr(address)}.')
+            # sc.info(f'Server accepted connection from {repr(address)}.')
             self.commif = CommIf(conn)
             super().__init__(completekey='tab', stdin=self.commif, stdout=self.commif)  # pyright: ignore
             SbotPdb.active_instance = self
 
         except (ConnectionError, socket.timeout) as e:
-            sc.info(f'Connection timed out: {str(e)}')
+            log(f'Connection timed out: {str(e)}')
+            # sc.info(f'Connection timed out: {str(e)}')
             sublime.message_dialog('Connection timed out, try again')
             self.do_quit()
 
         except Exception as e:
             # Other error handler. All are considered fatal. Exit the application. User needs to restart debugger.
-            sc.error(f'{type(e)} {str(e)}', e.__traceback__)
+            log(f'{type(e)} {str(e)}')
+            # sc.error(f'{type(e)} {str(e)}', e.__traceback__)
             if self.commif is not None:
                 self.commif.writeInfo(f'Server exception: {e}')
             self.do_quit()
 
     def breakpoint(self, frame):
         '''Starts the debugger.'''
-        sc.debug('breakpoint() entry')
+        log('breakpoint() entry')
+        # sc.debug('breakpoint() entry')
         if self.commif is not None:
             try:
                 # This blocks until user says done.
@@ -178,14 +200,17 @@ class SbotPdb(pdb.Pdb):
 
             except Exception as e:
                 # App exceptions actually go to sys.excepthook so this doesn't really do anything.
-                sc.error(f'{str(e)}', e.__traceback__)
+                log(f'{str(e)}')
+                # sc.error(f'{str(e)}', e.__traceback__)
 
-        sc.debug('breakpoint() exit')
+        log('breakpoint() exit')
+        # sc.debug('breakpoint() exit')
         self.do_quit()
 
     def do_quit(self, arg=None):
         '''Stopping debugging, clean up resources, exit application.'''
-        sc.info('Quitting.')
+        log('Quitting.')
+        # sc.info('Quitting.')
 
         if self.commif is not None:
             self.commif.close()
@@ -201,7 +226,17 @@ class SbotPdb(pdb.Pdb):
             super().do_quit(arg)
         except:
             pass
-        sc.debug('do_quit() exit')
+        # log('DBG do_quit() exit')
+
+
+#-----------------------------------------------------------------------------------
+def log(msg, tell=True):
+    with open(_log_fn, 'a') as log:
+        time_str = f'{str(datetime.datetime.now())}'[0:-3]
+        out_line = f'{time_str} {msg}'
+        log.write(out_line + '\n')
+        if tell:
+            print(msg)
 
 
 #-----------------------------------------------------------------------------------
--- sbot_pdb_client.py
+++ sbot_pdb_client.py
@@ -6,23 +6,20 @@ import threading
 import queue
 import datetime
 import traceback
+import json
 
 
 # Human polling time in msec.
-LOOP_TIME = 50
+_loop_time = 50
 
 # Server must reply to client in msec or it's considered dead.
-SERVER_RESPONSE_TIME = 200  # 100?
-
-# Some paths.
-PKGS_PATH = os.path.join(os.environ['APPDATA'], 'Sublime Text', 'Packages')
-LOG_FN = os.path.join(PKGS_PATH, 'User', 'PluginPdb', 'PluginPdb.log')
+_server_response_time = 200  # 100?
 
 # Delimiter for socket message lines.
-COMM_DELIM = '\n'
+_comm_delim = '\n'
 
-# Delimiter for log lines.
-LOG_EOL = '\n'
+# Where to log.
+_log_fn = '???'
 
 
 #-----------------------------------------------------------------------------------
@@ -40,16 +37,12 @@ class PdbClient(object):
         # Last command time. Non zero implies waiting for a response.
         self.sendts = 0
 
-        # Server config.
-        self.host = None
-        self.port = None
-        self.ind = None
-        self.get_settings()
+        # Server config. TODO1 get from cmd line too.
+        self.host = '127.0.0.1'
+        self.port = 59120
+        self.ind = '!'
 
         self.log_debug(f'Constructing client')
-
-        if self.host is None or self.port is None or self.ind is None:
-            self.log_error(NameError(f'Invalid settings: host:{self.host} port:{self.port} ind:{self.ind}'))
 
     def go(self):
         '''Run the main loop.'''
@@ -61,7 +54,7 @@ class PdbClient(object):
             ##### Run user cli input in a thread.
             def worker():
                 while run:
-                    self.cmdQ.put_nowait(sys.stdin.readline().replace(COMM_DELIM, ''))
+                    self.cmdQ.put_nowait(sys.stdin.readline().replace(_comm_delim, ''))
             threading.Thread(target=worker, daemon=True).start()
 
             ##### Forever loop #####
@@ -74,7 +67,7 @@ class PdbClient(object):
                     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
                     # Block with timeout.
-                    self.sock.settimeout(float(SERVER_RESPONSE_TIME) / 1000.0)
+                    self.sock.settimeout(float(_server_response_time) / 1000.0)
 
                     try:
                         self.sock.connect((self.host, self.port))
@@ -101,7 +94,7 @@ class PdbClient(object):
                 ##### Check for server not responding but still connected. #####
                 if self.commif is not None and self.sendts > 0:
                     dur = self.get_msec() - self.sendts
-                    if dur > SERVER_RESPONSE_TIME:
+                    if dur > _server_response_time:
                         self.log_info('Server not listening')
                         self.reset()
 
@@ -110,8 +103,8 @@ class PdbClient(object):
                     s = self.cmdQ.get()
 
                     if self.commif is not None:
-                        # self.log_debug(f'Send command: {make_readable(s)}')
-                        self.commif.write(s + COMM_DELIM)
+                        # self.log_debug(f'Send command: {self.make_readable(s)}')
+                        self.commif.write(s + _comm_delim)
                         self.commif.flush()
                         # Measure round trip for timeout.
                         self.sendts = self.get_msec()
@@ -133,7 +126,7 @@ class PdbClient(object):
                             else:
                                 sys.stdout.write(s)
                                 sys.stdout.flush()
-                                # self.log_debug(make_readable(s))
+                                # self.log_debug(self.make_readable(s))
                                 # Reset watchdog.
                                 self.sendts = 0
 
@@ -150,7 +143,7 @@ class PdbClient(object):
                         self.log_error(e)
 
                 ##### If there was no timeout, delay a bit. #####
-                slp = (float(LOOP_TIME) / 1000.0) if timed_out else 0
+                slp = (float(_loop_time) / 1000.0) if timed_out else 0
                 time.sleep(slp)
 
             self.log_debug('go() run ended')
@@ -164,30 +157,6 @@ class PdbClient(object):
             self.log_error(e)
 
         self.quit(0)
-
-    def get_settings(self):
-        '''Hand parse config files. Json parser is too heavy for this app.'''
-        # Overlay default and user options.
-        self.parse_settings_file(os.path.join(PKGS_PATH, 'SbotPdb', 'SbotPdb.sublime-settings'), True)
-        self.parse_settings_file(os.path.join(PKGS_PATH, 'User', 'SbotPdb.sublime-settings'), False)
-
-    def parse_settings_file(self, fn, required):
-        '''Parse one settings file. Get only the parts client is interested in.'''
-        if (not required and not os.path.isfile(fn)):
-            return
-
-        with open(fn) as f:
-            for s in f.readlines():
-                s = s.strip()
-                if s.startswith('\"'):
-                    s = s.replace('\"', '').replace(',', '')
-                    parts = s.split(':')
-                    name = parts[0].strip()
-                    val = parts[1].strip()
-
-                    if name == 'host': self.host = val
-                    elif name == 'port': self.port = int(val)
-                    elif name == 'internal_message_ind': self.ind = val
 
     def get_msec(self):
         '''Get current msec.'''
@@ -212,20 +181,40 @@ class PdbClient(object):
 
     def log_error(self, e):
         '''Log function. All error() are considered fatal.'''
-        write_log('ERR', str(e), e.__traceback__)
-        sys.stdout.write(f'{self.ind} Error: {e} - see the log{LOG_EOL}')
+        self.write_log('ERR', str(e), e.__traceback__)
+        sys.stdout.write(f'{self.ind} Error: {e}\n')
         sys.stdout.flush()
         self.quit(1)
 
     def log_info(self, msg):
         '''Log function.'''
-        write_log('INF', msg)
-        sys.stdout.write(f'{self.ind} {msg}{LOG_EOL}')
+        self.write_log('INF', msg)
+        sys.stdout.write(f'{self.ind} {msg}\n')
         sys.stdout.flush()
 
     def log_debug(self, msg):
         '''Log function.'''
-        write_log('DBG', msg)
+        self.write_log('DBG', msg)
+
+    def write_log(self, level, msg, tb=None):
+        '''Format a standard message with caller info and log it.'''
+
+        frame = sys._getframe(2)
+        fn = os.path.basename(frame.f_code.co_filename)
+        line = frame.f_lineno
+        time_str = f'{str(datetime.datetime.now())}'[0:-3]
+        # with open(_log_fn, 'a') as log:
+        #     out_line = f'{time_str} {level} {fn}:{line} {msg}'
+        #     log.write(out_line + '\n')
+        #     if tb is not None:
+        #         # The traceback formatter is a bit ugly - clean it up.
+        #         tblines = []
+        #         for s in traceback.format_tb(tb):
+        #             if len(s) > 0:
+        #                 tblines.append(s[:-1])
+        #         stb = '\n'.join(tblines)
+        #         log.write(stb + '\n')
+        #     log.flush()
 
     def quit(self, code):
         '''Clean up and go home.'''
@@ -235,49 +224,13 @@ class PdbClient(object):
             self.sock = None
         sys.exit(code)
 
-
-#-----------------------------------------------------------------------------------
-def write_log(level, msg, tb=None):
-    '''Format a standard message with caller info and log it.'''
-    frame = sys._getframe(2)
-    fn = os.path.basename(frame.f_code.co_filename)
-    line = frame.f_lineno
-
-    time_str = f'{str(datetime.datetime.now())}'[0:-3]
-
-    with open(LOG_FN, 'a') as log:
-        out_line = f'{time_str} {level} {fn}:{line} {msg}'
-        log.write(out_line + LOG_EOL)
-        if tb is not None:
-            # The traceback formatter is a bit ugly - clean it up.
-            tblines = []
-            for s in traceback.format_tb(tb):
-                if len(s) > 0:
-                    tblines.append(s[:-1])
-            stb = LOG_EOL.join(tblines)
-            log.write(stb + LOG_EOL)
-        log.flush()
-
-
-#-----------------------------------------------------------------------------------
-def excepthook(type, value, tb):
-    '''Process unhandled exceptions.'''
-
-    write_log('ERR', f'Unhandled exception {type.__name__}: {value}', tb)
-    sys.stdout.write(f'Unhandled exception {type.__name__}: {value} - see the log')
-    sys.exit(1)
-
-
-#-----------------------------------------------------------------------------------
-def make_readable(s):
-    '''So we can see things like LF, CR, ESC in log.'''
-    return s.replace('\n', '_N').replace('\r', '_R').replace('\033', '_E')
+    def make_readable(self, s):
+        '''So we can see things like LF, CR, ESC in log.'''
+        return s.replace('\n', '_N').replace('\r', '_R').replace('\033', '_E')
 
 
 #-----------------------------------------------------------------------------------
 if __name__ == '__main__':
-    # Connect the last chance hook.
-    sys.excepthook = excepthook
 
     client = PdbClient()
     client.go()
